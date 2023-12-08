80a5c9d6266ed961a4b060e1b53eaae8c1c04e86
Wanzhen Wang <101357076+WANGWANZHEnnn@users.noreply.github.com>
2023-12-08 12:08:38 +0000

    Add files via upload

--- employee.py
+++ employee.py
@@ -11,33 +11,35 @@ class Employee:
         self.contract_commission = contract_commission
         self.contracts_landed = contracts_landed
 
-    # Define a method to calculate total pay
-    def get_pay(self):
+    def calculate_base_pay(self):
         if self.contract_type == "salary":
-            pay = self.monthly_salary
-            if self.bonus_commission is not None:
-                pay += self.bonus_commission
+            return self.monthly_salary
         elif self.contract_type == "hourly":
-            pay = self.hourly_wage * self.hours_worked
-            if self.bonus_commission is not None:
-                pay += self.bonus_commission
-        if self.contract_commission is not None:
-            commission = self.contract_commission * self.contracts_landed
-            pay += commission
-        return pay
+            return self.hourly_wage * self.hours_worked
+        return 0
 
-    # Define __str__ method to explain pay calculation
+    def calculate_commission(self):
+        commission = 0
+        if self.bonus_commission is not None:
+            commission += self.bonus_commission
+        if self.contract_commission is not None and self.contracts_landed is not None:
+            commission += self.contract_commission * self.contracts_landed
+        return commission
+
+    def get_pay(self):
+        return self.calculate_base_pay() + self.calculate_commission()
+
     def __str__(self):
-        pay_details = f"{self.name} works on a"
+        pay_details = f"{self.name} works on a "
         if self.contract_type == "salary":
-            pay_details += f" monthly salary of {self.monthly_salary}"
-            if self.bonus_commission is not None:
-                pay_details += f" and receives a bonus commission of {self.bonus_commission}"
+            pay_details += f"monthly salary of {self.monthly_salary}"
         elif self.contract_type == "hourly":
-            pay_details += f" contract of {self.hours_worked} hours at {self.hourly_wage}/hour"
-            if self.bonus_commission is not None:
-                pay_details += f" and receives a bonus commission of {self.bonus_commission}"
+            pay_details += f"contract of {self.hours_worked} hours at {self.hourly_wage}/hour"
+        
+        if self.bonus_commission is not None:
+            pay_details += f" and receives a bonus commission of {self.bonus_commission}"
         if self.contract_commission is not None:
             pay_details += f" and receives a commission for {self.contracts_landed} contract(s) at {self.contract_commission}/contract"
+        
         total_pay = self.get_pay()
         return f"{pay_details}. Their total pay is {total_pay}."
